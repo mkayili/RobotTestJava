f750127208e4876af847e581adeb8509b7791e64
kaan <kaanaltinay00@gmail.com>
2019-07-04 14:34:29 +0300

    Proje test metodlarına ayrıldı

---
+++ src/main/java/Modules/SideBar/approveAwaitingTest.java
@@ -0,0 +1,33 @@
+package Modules.SideBar;
+
+import Report.Reports;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+
+public class approveAwaitingTest {
+
+    AndroidDriver driver;
+
+    public approveAwaitingTest(AndroidDriver driver) {
+        this.driver = driver;
+    }
+
+    public void approveAwaiting() {
+        try {
+            MobileElement awaiting = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View[2]/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[1]/android.widget.TextView[2]\n");
+            int aw = Integer.parseInt(getInsideParanthesis(awaiting.getText()));
+            awaiting.click();
+            // awaiting elemanlar�n�n say�s� kontrol edilecek
+
+        } catch (Exception e) {
+            Reports.report("Menu action Failed", "approveAwaiting", "approveAwaiting failed...");
+        }
+    }
+
+    public String getInsideParanthesis(String str) {
+        String answer = str.substring(str.indexOf("(") + 1, str.indexOf(")"));
+        return answer;
+    }
+
+}
---
+++ src/main/java/Modules/SideBar/buttonCheck.java
@@ -0,0 +1,46 @@
+package Modules.SideBar;
+
+import Report.Reports;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.NoSuchElementException;
+
+
+public class buttonCheck {
+    AndroidDriver driver;
+
+    public buttonCheck(AndroidDriver driver) {
+        this.driver = driver;
+    }
+
+
+    public void openMenu() {
+        try {
+            MobileElement menubutton = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.TextView");
+            menubutton.click();
+            allButtons();
+        } catch (NoSuchElementException e) {
+            Reports.report("NoElement", "openMenu", "Menu iconu bu ekranda mevcut de�il...");
+        }
+    }
+
+    public void allButtons() {
+        try {
+            MobileElement awaiting = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View[2]/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[1]/android.widget.TextView[2]\n");
+            MobileElement approved = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View[2]/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[2]/android.widget.TextView[2]\n");
+            MobileElement rejected = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View[2]/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[3]/android.widget.TextView[2]\n");
+            MobileElement search = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View[2]/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[4]/android.widget.TextView[2]\n");
+            MobileElement syncPhotos = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View[2]/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[5]/android.widget.TextView[2]\n");
+            MobileElement Support = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View[2]/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[6]/android.widget.TextView[2]\n");
+            MobileElement educationDocument = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View[2]/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[7]/android.widget.TextView[2]\n");
+            MobileElement faq = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View[2]/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[8]/android.widget.TextView[2]\n");
+            MobileElement clear = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View[2]/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[9]/android.widget.TextView[2]\n");
+            //approveAwaitingTest();
+            //approvedTest();
+            //rejectedTest();
+            //searchTest();
+        } catch (NoSuchElementException e) {
+            Reports.report("NoElement", "sideBar", "Menude eksik button var...");
+        }
+    }
+}
