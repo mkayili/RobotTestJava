263c34ec388bb076c732e486743666c6e2b40c3f
kaan <kaanaltinay00@gmail.com>
2019-07-19 11:58:05 +0300

    -ClearData changed. -SideBar functions changed -forgotPassword test added to login

---
+++ src/main/java/Modules/HomePage/CustomerUpdates/NewOutlets/newOutlet.java
@@ -0,0 +1,28 @@
+package Modules.HomePage.CustomerUpdates.NewOutlets;
+
+import Report.Reports;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.NoSuchElementException;
+
+import java.util.concurrent.TimeUnit;
+
+public class newOutlet {
+    AndroidDriver driver;
+
+    public newOutlet(AndroidDriver driver) {
+        this.driver = driver;
+    }
+
+    public void runTest() {
+
+        try {
+            driver.manage().timeouts().implicitlyWait(175, TimeUnit.SECONDS);
+            MobileElement CustomerUpdateButton = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.support.v4.widget.DrawerLayout/android.view.View/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[6]\n");
+            CustomerUpdateButton.click();
+            Reports.report("OK", "CustomerUpdate", "activateOutlet butonu ekranda mevcut. Butona tıklandı ...");
+        } catch (NoSuchElementException e) {
+            Reports.report("NoElement", "CustomerUpdate", "activateOutlet butonuna ulaşılamadı.");
+        }
+    }
+}
